d89411d8347849cf41ce386e46ae83fd76a16f29
Trinity-SYT-SECURITY <sytyactfhaha@gmail.com>
2022-05-03 05:31:34 +0800

    PWN

--- pass/pass.c
+++ pass/pass.c
@@ -1,19 +1,23 @@
-#include"stdio.h"
-#include"stdlib.h"
+#include "stdio.h"
+#include "stdlib.h"
 
-void printTheKey(){
+void printTheKey()
+{
   /*
    *
    * print the key
    *
    */
 }
-
-int main(){
+//這函式註解掉，也許在原binary中會有它印KEY的過程
+int main()
+{
   setvbuf(stdout, 0, 2, 0);
   setvbuf(stdin, 0, 2, 0);
   int token = 1234;
-  char key[16];
+  char key[16]; // key的長度只有16個字
+
+  //一開始會輸出一些簡單的敘述
 
   printf("Billy left his key in the locked room.\n");
   printf("However, he forgot the token of the room.\n");
@@ -21,11 +25,19 @@ int main(){
 
   read(0, key, 40);
 
-  if((int)token == 0xdeadbeef){
+  /*
+  read讀到key的variable,但是key的長度只有16個字，卻限制了讀的長度可以有40個字
+  */
+
+  if ((int)token == 0xdeadbeef)
+  { //確認token是否等於0xdeadbeef的值
+    //基本上token不會等於0xdeadbeef，也就讓if的判斷式下不會成立
     printf("Door open. OwO\n");
-    printTheKey();
-    system("cat /home/ctf/flag");
-  }else{
+    printTheKey();                //有成立就會進到這個function
+    system("cat /home/ctf/flag"); //這個部分應該是在gohome那題的
+  }
+  else
+  {
     printf("Cannot open door. QwQ\n");
   }
 
